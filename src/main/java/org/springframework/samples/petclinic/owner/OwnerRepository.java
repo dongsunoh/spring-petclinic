d22ad70e8f839b8025630a3f51f78f3ce5fa73ca
Dongsun Oh <dongsun.oh@gmail.com>
2019-04-01 21:50:32 +0200

    developed search firstName function

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -15,12 +15,12 @@
  */
 package org.springframework.samples.petclinic.owner;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
 
 /**
  * Repository class for <code>Owner</code> domain objects All method names are compliant with Spring Data naming
@@ -37,16 +37,18 @@ public interface OwnerRepository extends Repository<Owner, Integer> {
     /**
      * Retrieve {@link Owner}s from the data store by last name, returning all owners
      * whose last name <i>starts</i> with the given name.
+     *
      * @param lastName Value to search for
      * @return a Collection of matching {@link Owner}s (or an empty Collection if none
      * found)
      */
-    @Query("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.lastName LIKE :lastName%")
+    @Query("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.lastName LIKE %:lastName%")
     @Transactional(readOnly = true)
     Collection<Owner> findByLastName(@Param("lastName") String lastName);
 
     /**
      * Retrieve an {@link Owner} from the data store by id.
+     *
      * @param id the id to search for
      * @return the {@link Owner} if found
      */
@@ -56,9 +58,16 @@ public interface OwnerRepository extends Repository<Owner, Integer> {
 
     /**
      * Save an {@link Owner} to the data store, either inserting or updating it.
+     *
      * @param owner the {@link Owner} to save
      */
     void save(Owner owner);
 
+    @Query("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.firstName LIKE %:firstName%")
+    @Transactional(readOnly = true)
+    Collection<Owner> findByFirstName(@Param("firstName") String firstName);
 
+    @Query("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.firstName LIKE %:firstName% AND owner.lastName LIKE %:lastName%")
+    @Transactional(readOnly = true)
+    Collection<Owner> findByFirstNameAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
